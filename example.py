2461765f99fe5401d62f930bf7b5a358cec4066c
Cong Zhang <cong1.zhang@intel.com>
2026-03-05 17:54:19 +0800

    update example.py

--- example.py
+++ example.py
@@ -8,10 +8,10 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(path)
     llm = LLM(path, enforce_eager=True, tensor_parallel_size=1, gpu_memory_utilization=0.5, max_model_len=2048)
 
-    sampling_params = SamplingParams(temperature=0.6, max_tokens=32)
+    sampling_params = SamplingParams(temperature=0.6, max_tokens=256)
     prompts = [
-        "hello",
-        "hi",
+        "introduce yourself",
+        "list all prime numbers within 100"
     ]
     prompts = [
         tokenizer.apply_chat_template(
